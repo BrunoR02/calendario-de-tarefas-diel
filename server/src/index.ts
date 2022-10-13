c4cba70b8926f7e3ca293672a5cd039b8372b317
Bruno <brunolucas23@gmail.com>
2022-10-13 13:23:38 -0300

    Ordered Task List by date and time

--- server/src/index.ts
+++ server/src/index.ts
@@ -16,7 +16,48 @@ type TaskType = {
   date: string
 }
 
-const TASK_LIST:TaskType[] = []
+const TASK_LIST:TaskType[] = [
+  {
+    id: "123",
+    title: "Desenvolver tal coisa",
+    description: "Atualizr acoisas akmdao aoskd",
+    startTime: "08:30",
+    endTime: "09:30",
+    date: "10/10/2022"
+  },
+  {
+    id: "133",
+    title: "Desenvolver aplicativo ate segunda",
+    description: "Precisa terminar a tempo",
+    startTime: "12:00",
+    endTime: "13:30",
+    date: "12/10/2022"
+  },
+  {
+    id: "1113",
+    title: "Desenvolver site",
+    description: "Sim site bonito",
+    startTime: "10:00",
+    endTime: "12:30",
+    date: "12/10/2022"
+  },
+  {
+    id: "12113",
+    title: "Desenvolver site",
+    description: "Sim site bonito",
+    startTime: "15:00",
+    endTime: "15:30",
+    date: "12/10/2022"
+  },
+  {
+    id: "11131",
+    title: "Desenvolver site",
+    description: "Sim site bonito",
+    startTime: "13:00",
+    endTime: "14:30",
+    date: "11/10/2022"
+  }
+]
 
 app.get("/api",(req,res)=>{
   res.send("Funcionando")
@@ -33,7 +74,12 @@ app.post("/api/task",(req,res)=>{
 })
 
 app.get("/api/task",(req,res)=>{
-  res.json(TASK_LIST)
+  const orderedTasks = TASK_LIST.sort((a,b)=>{
+    if(a.date > b.date || (a.date === b.date && a.startTime > b.startTime)){ return 1 }
+    else{ return -1}
+  })
+  
+  res.json(orderedTasks)
 })
 
 app.listen(PORT,()=>{
