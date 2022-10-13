6a5e1d2f63be837ef7bfbe9c03f54dbd8dd028c1
Bruno <brunolucas23@gmail.com>
2022-10-13 06:30:01 -0300

    Created API Route to post the tasks

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,8 +3,32 @@ const app = express()
 
 const PORT = process.env.PORT || 3001
 
-app.get("/",(req,res)=>{7
+type TaskType = {
+  title: string
+  description: string
+  startTime: string
+  endTime: string
+  date: string
+}
+
+const TASK_LIST:TaskType[] = []
+
+app.get("/api",(req,res)=>{
   res.send("Funcionando")
+})
+
+app.post("/api/task",(req,res)=>{
+  const task:TaskType = req.body
+  if(!task){
+    res.sendStatus(400)
+  } else {
+    TASK_LIST.push(task)
+    res.status(201)
+  }
+})
+
+app.get("/api/task",(req,res)=>{
+  res.json(TASK_LIST)
 })
 
 app.listen(PORT,()=>{
